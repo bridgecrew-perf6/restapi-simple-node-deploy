aede8d3522e085b67d600c0c37fbef6513a7718a
marksxp <marksxp@gmail.com>
2022-03-31 19:59:32 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world with express')
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log('server on port %d', port)
+})
+
+/*
+node src/index.js
+npm start
+
+GET
+http://localhost:3000/
+*/
